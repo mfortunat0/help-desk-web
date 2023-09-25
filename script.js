b6b57a61d1b86d9b41dc19167c5ec3eaa2da8369
mfortunat0 <matheus.xmaz10@gmail.com>
2023-09-25 15:07:22 -0300

    Adicionando modal de chamado e suas funcionalidades

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello!');
+const inputElement = document.querySelector('#customFile');
+const inputLabel = document.querySelector('#customFileLabel');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonModal = document.querySelector('#buttonModal');
+
+inputElement.addEventListener('change', () => {
+  if (inputElement.files.length > 0) {
+    console.log('asdas');
+    inputLabel.classList.add('custom-file-label-confirmed');
+    inputLabel.innerHTML = 'Anexado';
+  } else {
+    inputLabel.classList.remove('custom-file-label-confirmed');
+  }
+});
+
+overlay.addEventListener('click', () => {
+  modal.style.display = 'none';
+  overlay.style.display = 'none';
+});
+
+buttonModal.addEventListener('click', () => {
+  modal.style.display = 'block';
+  overlay.style.display = 'block';
+});
